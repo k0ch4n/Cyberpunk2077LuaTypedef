885bc7a9073cc447f855d722662128cecc8a01ff
infinitY0369 <flutist.sud.0v@icloud.com>
2024-02-27 22:56:04 +0900

    Update tools.

--- Run.ps1
+++ Run.ps1
@@ -1,6 +1,8 @@
-param ([switch]$Run, [switch]$Clean)
+param ([switch]$Clean, [switch]$Dry)
 
 Set-Location -Path $PSScriptRoot
+
+$IsVenv = $null -ne $env:VIRTUAL_ENV
 
 if ($Clean -and (Test-Path -Path ".venv")) {
     Remove-Item -Path ".venv" -Recurse -Force
@@ -15,6 +17,12 @@ if (-not (Test-Path -Path ".venv")) {
 pip list --outdated | Select-Object -Skip 2 | ForEach-Object { ($_ -split "\s+")[0] } | ForEach-Object { py -m pip install --upgrade $_ --no-cache-dir }
 py -m pip install --requirement "requirements.txt" --upgrade --no-cache-dir
 
-if ($Run) {
+if (-not $Dry) {
     py -B ".\src\main.py"
 }
+
+if (-not $IsVenv) {
+    deactivate
+}
+
+Set-Location -Path -
