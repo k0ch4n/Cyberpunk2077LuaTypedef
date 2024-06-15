60f0d72111d212a9bb4afdab4e03395e3a1a7c6c
infinitY0369 <flutist.sud.0v@icloud.com>
2024-06-15 17:39:48 +0900

    Update Codeware

---
+++ dist/library/Codeware/Generated/classes/CMeshMaterialEntry.lua
@@ -0,0 +1,11 @@
+---@meta
+
+
+---@class CMeshMaterialEntry
+---@field material IMaterial
+CMeshMaterialEntry = {}
+
+
+---@param fields? CMeshMaterialEntry
+---@return CMeshMaterialEntry
+function CMeshMaterialEntry.new(fields) end
---
+++ dist/library/Codeware/Generated/classes/entMeshComponent.lua
@@ -0,0 +1,11 @@
+---@meta
+
+
+---@class entMeshComponent: entIVisualComponent
+---@field meshResource CMesh
+entMeshComponent = {}
+
+
+---@param fields? entMeshComponent
+---@return entMeshComponent
+function entMeshComponent.new(fields) end
